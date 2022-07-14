3cd6c93b00600469bc40d6cef23ab6abe038ad59
wrjang96 <wrjang@naver.com>
2022-07-14 11:08:09 +0900

    asd

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import List from "./pages/List/List";
 import Detail from "./pages/Detail/Detail";
 import Order from "./pages/Order/Order";
 import "./App.css";
+//sda
 
 function App() {
   return (
