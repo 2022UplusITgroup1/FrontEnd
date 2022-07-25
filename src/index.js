3010db26b50e20d6a9a0f8d91dc533bceeac1070
avocado51 <ujmikzz7@hanmail.net>
2022-07-25 21:18:17 +0900

    Add localstorage clear when jsessionid is changed

--- src/index.js
+++ src/index.js
@@ -14,6 +14,6 @@ root.render(
   </CookiesProvider>
 );
 
-window.addEventListener("beforeunload", (e) => {
-  localStorage.clear();
-});
+// window.addEventListener("beforeunload", (e) => {
+//   localStorage.clear();
+// });
