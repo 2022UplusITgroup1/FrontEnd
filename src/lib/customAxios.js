4b31c7c1b05953da3e561fb054720f02c1c33f4c
Seung Hyun <jsh5408@naver.com>
2022-07-28 05:10:07 +0900

    Modify axios url

--- src/lib/customAxios.js
+++ src/lib/customAxios.js
@@ -4,10 +4,10 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 const customAxios = axios.create({
-  //   baseURL:
-  //     "http://a1eee0639847a49a980fdc126dd70a9b-1394049333.ap-northeast-2.elb.amazonaws.com:8000",
+  baseURL:
+    "http://a1eee0639847a49a980fdc126dd70a9b-1394049333.ap-northeast-2.elb.amazonaws.com:8000",
   //   baseURL: "http://localhost:8000",
-  baseURL: "http://43.200.122.174:8000",
+  //   baseURL: "http://43.200.122.174:8000",
   // "http://a1eee0639847a49a980fdc126dd70a9b-1394049333.ap-northeast-2.elb.amazonaws.com:8000",
   //   "http://43.200.122.174:8000",
   // "http://a1eee0639847a49a980fdc126dd70a9b-1394049333.ap-northeast-2.elb.amazonaws.com:8000",
