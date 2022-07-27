3fd197e20182ab38244b6998076cae4e428f1969
Seung Hyun <jsh5408@naver.com>
2022-07-27 20:38:36 +0900

    Modify axios -> fetch

--- src/lib/customAxios.js
+++ src/lib/customAxios.js
@@ -11,11 +11,15 @@ const customAxios = axios.create({
     "http://a1eee0639847a49a980fdc126dd70a9b-1394049333.ap-northeast-2.elb.amazonaws.com:8000",
   headers: {
     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8;",
+    // Cache: "no-cache",
     // "Access-Control-Allow-Origin": "*",
     // "Access-Control-Allow-Headers": "*",
     // "Access-Control-Allow-Credentials": "true",
     // withCredentials: true,
   },
+  //   credentials: "same-origin",
+  //   mode: "same-origin",
+  //   withCredentials: true,
 });
 
 export default customAxios;
